83e26b329c76638119768dafbcae0acf26ff458c
RafaelMainieri <rafasolmainieri@gmail.com>
2024-05-25 14:08:24 -0300

    etapa1

---
+++ etapa1/Produto.java
@@ -0,0 +1,33 @@
+package lab1.zDesafio.etapa1;
+
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome = " + nome + '\n' +
+               "Preço = " + preco;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
